af61711de9d7c00733c096cb3003a9f187027f9c
mit-anuj <anujmittal0040@gmail.com>
2023-01-31 21:07:41 +0530

    recursion

---
+++ print_keypad_combination.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class print_keypad_combination {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter combination: ");
+        String combination=sc.next();
+        printKC(combination,"");
+
+        sc.close();
+    }
+    public static String[] codes={",;","abc","def","ghi","jkl","mno","pqrs","tu","VWX","yz"};
+    public static void printKC(String str,String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char ch=str.charAt(0);
+        String rem=str.substring(1);
+        String code=codes[ch-'0'];
+        for(int i=0;i<code.length();i++){
+            printKC(rem,ans+"");
+            printKC(rem,ans+code.charAt(i));
+            
+        }
+    }
+}
--- print_sequence.java
+++ print_sequence.java
@@ -11,14 +11,14 @@ public class print_sequence {
     }
 
     public static void printSequence(String str, String ans){
-        if(str.length()==0){
-            System.out.println(ans);
+        if(str.length()==0){// base condition.
+            System.out.println(ans);// this will print the sequence.
             return;
         }
-        char ch=str.charAt(0);
-        String subs=str.substring(1);
-        printSequence(subs, ans+"");
-        printSequence(subs, ans+ch);
+        char ch=str.charAt(0);// this will get the first character.
+        String subs=str.substring(1);// this will store the remaining characters of the string.
+        printSequence(subs, ans+"");// this condition will add blank.
+        printSequence(subs, ans+ch);// this condition will add ch in fornt of the answer.
         
     }
 }
