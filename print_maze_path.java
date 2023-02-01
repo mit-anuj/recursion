c136ccdf6133eb6d758e89d36c2c3091a4deecab
mit-anuj <anujmittal0040@gmail.com>
2023-02-01 12:18:00 +0530

    Update print_maze_path.java

--- print_maze_path.java
+++ print_maze_path.java
@@ -12,16 +12,16 @@ public class print_maze_path {
 
     }
     public static void printMP(int cr,int cc,int tr,int tc, String ans){
-        if(cr== tr && cc== tc)
+        if(cr== tr && cc== tc)// base case.
         {
             System.out.println(ans);
             return;
         }
         if(cc<tc){
-            printMP(cr,cc+1,tr,tc,ans+ "h");
+            printMP(cr,cc+1,tr,tc,ans+ "h");// call for horizontal movement.
         }
         if(cr<tr){
-            printMP(cr+1,cc,tr,tc,ans+"v");
+            printMP(cr+1,cc,tr,tc,ans+"v");// call for vertical movement.
         }
     }
 }
